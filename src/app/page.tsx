81d8f194295b33bc3707a34a48d9bb8ddc23fab5
Opal Aja <opank2441@gmail.com>
2025-05-27 16:17:03 +0000

    kenapa masih belum bisa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 
+
 import AppLayout from '@/components/layout/AppLayout';
 
 export default function HomePage() {
   return (
-    <main>
-      <AppLayout>{/* Children passed here will be handled by AppLayout */}</AppLayout>
-    </main>
+    <AppLayout>{/* Children passed here will be handled by AppLayout */}</AppLayout>
   );
 }
+
