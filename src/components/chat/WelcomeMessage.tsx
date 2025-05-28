b621c4cb3e48d4a04134923291aae603492715e0
opank aja <opankaje0991@gmail.com>
2025-05-28 16:10:28 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <WelcomeMessage>
    
    Relevant files:
    
        - https://6000-firebase-studio-1748362646702.cluster-73qgvk7hjjadkrjeyexca5ivva.cloudworkstations.dev/_next/static/chunks/node_modules_ef03982d._.js
        - https://6000-firebase-studio-1748362646702.cluster-73qgvk7hjjadkrjeyexca5ivva.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-firebase-studio-1748362646702.cluster-73qgvk7hjjadkrjeyexca5ivva.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    dimana tambahkan tulisan lu gabut? yaa ngecet lah

--- src/components/chat/WelcomeMessage.tsx
+++ src/components/chat/WelcomeMessage.tsx
@@ -2,15 +2,14 @@
 "use client";
 
 import { AppLogo } from "@/components/core/AppLogo";
-import { MessageSquareDashed } from "lucide-react";
 
 export function WelcomeMessage() {
   return (
     <div className="flex flex-col items-center justify-center flex-1 text-center p-8 bg-background">
-      
-      <AppLogo className="w-10 h-10 text-primary mb-2" />
+      <AppLogo className="w-10 h-10 text-primary mb-4" />
+      <p className="text-lg text-muted-foreground">
+        lu gabut? yaa ngecet lah
+      </p>
     </div>
   );
 }
-
-    
